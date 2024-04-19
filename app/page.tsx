6a6953af53a2fe2b20a2f4c764359cf8e1496eaa
silver613 <bestwing915@gmail.com>
2024-04-19 06:39:46 +0100

    fixed useGetRestaurant end point

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
+"use client"
 import Hero from "@/components/Hero";
 import FoodDiscoveryPage from "@/components/FoodDiscoveryPage";
-import React from "react";
+import React, { useEffect } from "react";
 import PopularDishes from "@/components/PopularDishes";
 import { Separator } from "@/components/ui/separator";
 import Reviews from "@/components/Reviews";
 import Touch from "@/components/Touch";
+import { useGetRestaurant } from "@/api/RestaurantApi";
 
 const page = () => {
+  const { restaurant } = useGetRestaurant();
+
+  useEffect(() => {
+    restaurant    
+  }, [useGetRestaurant, restaurant])
   return (
     <div className="bg-gray-50 dark:bg-[#181c20] dark:text-white overflow-hidden">
       <Hero />
